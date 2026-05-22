f7a291de5c4a494941dd18c3e84884fad27ec84a
Marvin Sum <831708+marvinsum@users.noreply.github.com>
2026-05-21 21:32:39 -0400

    Add @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     build: {
         outDir: "dist",
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
     test: {
         environment: "node",
